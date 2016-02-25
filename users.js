260cf6f8c4062c817c5cd3eb89f5d2fcd3709966
Peter Mikitsh <peter.mikitsh@gmail.com>
2016-02-24 23:34:20 -0800

    custom email prefixes
    use environment variable to prefix email addresses

--- users.js
+++ users.js
@@ -4,28 +4,28 @@ module.exports = {
       testuser1: {
         username: 'testuser1',
         password: 'password1',
-        email: 'testuser1@test.com',
+        email: process.env.MAIL_PREFIX + 'test+1@gmail.com',
         firstName: 'John',
         lastName: 'Doe'
       },
       testuser2: {
         username: 'testuser2',
         password: 'password2',
-        email: 'testuser2@test.com',
+        email: process.env.MAIL_PREFIX + 'test+2@gmail.com',
         firstName: 'Jane',
         lastName: 'Doe'
       },
       testuser3: {
         username: 'testuser3',
         password: 'password3',
-        email: 'testuser3@test.com',
+        email: process.env.MAIL_PREFIX + 'test+3@gmail.com',
         firstName: 'Judy',
         lastName: 'Doe'
       },
       testuser4: {
         username: 'testuser4',
         password: 'password4',
-        email: 'testuser4@test.com',
+        email: process.env.MAIL_PREFIX + 'test+4@gmail.com',
         firstName: 'Joseph',
         lastName: 'Doe'
       }
